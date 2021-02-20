f189c9df85dc93b35e6aad4afa08d4b8977131cb
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2021-02-20 21:13:19 +0000

    Bump version: 0.4.0 → 0.5.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ test_requirements = [
 ]
 setup(
     name='gnosis_epl',
-    version='0.4.0',
+    version='0.5.0',
     description="Gnosis EPL python package",
     long_description=readme,
     author="Felipe Arruda Pontes",
