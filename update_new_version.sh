0826311a895b439deead8352d6f23fcdf75f9737
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2023-02-21 11:09:25 +0000

    fixing again

--- update_new_version.sh
+++ update_new_version.sh
@@ -7,11 +7,12 @@ CUR_VERSION=$(cat setup.cfg | grep "current_version = .*" | awk '{split($0,a,"\=
 echo "new version: ${CUR_VERSION}"
 echo "copying to ${HEROKU_PYPI_LOCAL_DIR}/data/"
 cp dist/gnosis_epl-${CUR_VERSION}.tar.gz ${HEROKU_PYPI_LOCAL_DIR}/data/
-pushd $HEROKU_PYPI_LOCAL_DIR
-echo "commiting changes in pypi repo"
-git add data/
-git commit -m 'new version of gnosis_epl'
-echo "pushing changes to heroku pipy master"
-git push origin master
+# pushd $HEROKU_PYPI_LOCAL_DIR
+# echo "commiting changes in pypi repo"
+# git add data/
+# git commit -m 'new version of gnosis_epl'
+# echo "pushing changes to heroku pipy master"
+# git push origin master
 echo "You have to upload the new dist 'gnosis_epl-${CUR_VERSION}.tar.gz' file into the pythonanywhere web app usign the browser"
-popd
+echo "and also commit the new dist file in the heroku repo"
+# popd
